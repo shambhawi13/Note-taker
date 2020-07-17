ef00b2cb930bf9ac813c979f38a54f5e6d515fa4
Sham <shambhawi.kumari1393@gmail.com>
2020-07-17 09:45:00 -0700

    initial skeleton

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Note-taker
+
+Deployed link: https://secure-beyond-12599.herokuapp.com/
+git heroku link:  https://git.heroku.com/secure-beyond-12599.git
+
+git repository: https://github.com/shambhawi13/Note-taker 
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+//const mysql = require('mysql');
+const path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/notes/:id", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
